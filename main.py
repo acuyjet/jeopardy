f8e93f058a5a1fbb3dfd2ea03a074a608afe190a
acuyjet <allison@cuyjet.com>
2020-07-17 21:54:07 -0400

    Add todo.txt and intro text

---
+++ main.py
@@ -0,0 +1,17 @@
+
+# Prompt user for name
+player_name = input("Hello! Please enter your name as you want it displayed on the monitor on the front of your podium: ")
+print("\nThanks, {}! Let's play Jeopardy!".format(player_name))
+print("\nRemember to phrase your response in the form of a question!\nEnter 'Q' at any time to quit.")
+
+while True:
+	pass
+# Display clue and prompt user for response
+
+# If response is correct, add to score
+
+# If response is incorrect, subtract from score
+
+# Allow user to break out of loop and answer Final Jeopardy clue
+
+# Display total score and add to leaderboard
