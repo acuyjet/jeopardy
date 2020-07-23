874586fb1e4d3a0db79569ec4394d4356dbee330
acuyjet <allison@cuyjet.com>
2020-07-23 18:34:55 -0400

    Update gitignore and get game to stop displaying the same clue each time

--- game.py
+++ game.py
@@ -1,7 +1,12 @@
 import requests
 
-api_response = requests.get('https://jservice.io/api/random').json()
 player_score = 0
+
+def get_clue():
+	# Display clue
+	api_response = requests.get('https://jservice.io/api/random').json()
+	print("{} for ${}: {}\n".format(api_response[0]['category']['title'].upper(
+        ), api_response[0]['value'], api_response[0]['question'].upper()))
 
 # Prompt user for name
 player_name = input("Hello! Please enter your name as you want it displayed on the monitor on the front of your podium: ")
@@ -9,42 +14,44 @@ print("\nThanks, {}! Let's play Jeopardy!".format(player_name))
 print("\nRemember to phrase your response in the form of a question!\nEnter 'S' to skip a clue, and 'Q' at any time to quit.\n")
 
 while True:
-	# Display clue and prompt user for response
-	response = input("{} for ${}: {}\n".format(api_response[0]['category']['title'].upper(
-        ), api_response[0]['value'], api_response[0]['question']))
 
-	if response.upper() == api_response[0]['answer'].upper():
-		player_score += api_response[0]['value']
-		print("Correct! You have ${}.".format(player_score))
-	else:
-		# If response is incorrect, subtract from score
-		player_score -= api_response[0]['value']
-		print("Oh, sorry! Incorrect. You have ${}.".format(player_score))
+	get_clue()
+
+	# Prompt user for response
+	response = input("> ")
+
+# 	if response.upper() == api_response[0]['answer'].upper():
+# 		player_score += api_response[0]['value']
+# 		print("Correct! You have ${}.".format(player_score))
+# 	else:
+# 		# If response is incorrect, subtract from score
+# 		player_score -= api_response[0]['value']
+# 		print("Oh, sorry! Incorrect. You have ${}.".format(player_score))
 	
-	# User can enter 'S' to skip a clue 
-	# If response is correct, add to score
+# 	# User can enter 'S' to skip a clue 
+# 	# If response is correct, add to score
 
-		# Allow user to break out of loop and answer Final Jeopardy clue
-	if response.upper() == "Q":
-		play_final = input(
-			"Before you go, would you like to play Final Jeopardy? Y/N ")
-		if play_final.upper() == "Y":
-			if player_score <= 0:
-				print("Sorry, you don't have enough to play, and you'll have to leave the stage in shame.")
-				break
-			final_wager = input(
-				"How much would you like to wager? You have {}".format(player_score))
-			response = input("{} for ${}: {}\n".format(api_response[0]['category']['title'].upper(
-			), api_response[0]['value'], api_response[0]['question']))
-			if response.upper() == api_response[0]['answer'].upper():
-				player_score += final_wager
-				print("Correct! Thanks for playing. Your final score is ${}.".format(player_score))
-				break
-			else:
-				player_score -= final_wager
-				print("Oh, sorry! Incorrect. Your final score is ${}.".format(player_score))
-				break
+# 		# Allow user to break out of loop and answer Final Jeopardy clue
+# 	if response.upper() == "Q":
+# 		play_final = input(
+# 			"Before you go, would you like to play Final Jeopardy? Y/N ")
+# 		if play_final.upper() == "Y":
+# 			if player_score <= 0:
+# 				print("Sorry, you don't have enough to play, and you'll have to leave the stage in shame.")
+# 				break
+# 			final_wager = input(
+# 				"How much would you like to wager? You have {}".format(player_score))
+# 			response = input("{} for ${}: {}\n".format(api_response[0]['category']['title'].upper(
+# 			), api_response[0]['value'], api_response[0]['question']))
+# 			if response.upper() == api_response[0]['answer'].upper():
+# 				player_score += final_wager
+# 				print("Correct! Thanks for playing. Your final score is ${}.".format(player_score))
+# 				break
+# 			else:
+# 				player_score -= final_wager
+# 				print("Oh, sorry! Incorrect. Your final score is ${}.".format(player_score))
+# 				break
 
 	
 
-# Display total score and add to leaderboard
+# # Display total score and add to leaderboard
