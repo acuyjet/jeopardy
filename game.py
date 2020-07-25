ad89db3d39682a21523e19e728a91b302feae0d1
acuyjet <allison@cuyjet.com>
2020-07-25 16:54:53 -0400

    Add error handling for TypeError when clue value is

--- game.py
+++ game.py
@@ -11,22 +11,41 @@ print("\nRemember to phrase your response in the form of a question!\nEnter 'S' 
 while True:
     # Get clue
     api_response = requests.get('https://jservice.io/api/random').json()
-    print("{} for ${}: {}\n".format(api_response[0]['category']['title'].upper(
+    print("\n{} for ${}: {}\n".format(api_response[0]['category']['title'].upper(
     ), api_response[0]['value'], api_response[0]['question'].upper()))
     # Prompt user for response
-    response = input("('S' to skip clue, 'Q' to quit) ")
+    response = input("('S' to skip clue, 'Q' to quit)\n> ")
 
     if response.upper() == api_response[0]['answer'].upper():
         # If response is correct, add to score
         player_score += api_response[0]['value']
-        print("Correct! You have ${}.\n".format(player_score))
+        print("\nCorrect! You have ${}.\n".format(player_score))
     # User can enter 'S' to skip a clue
     elif response.upper() == 'S':
         pass
     # Allow user to break out of loop and answer Final Jeopardy clue
     elif response.upper() == 'Q':
+        play_final = input("Before you go, would you like to play Final Jeopardy? Y/N ")
+        if play_final.upper() == "Y":
+            if player_score <= 0:
+                print("\nSorry, you don't have enough to play, and you'll have to leave the stage in shame.\n")
+                break
+        # 	final_wager = input("How much would you like to wager? You have {}".format(player_score))
+        #     response = input("{} for ${}: {}\n".format(api_response[0]['category']['title'].upper(), api_response[0]['value'], api_response[0]['question']))
+        #     if response.upper() == api_response[0]['answer'].upper():
+        # 		player_score += final_wager
+        # 		print("Correct! Thanks for playing. Your final score is ${}.".format(player_score))
+        # 		break
+        # 	else:
+        # 		player_score -= final_wager
+        # 		print("Oh, sorry! Incorrect. Your final score is ${}.".format(player_score))
+        # 		breakcler
         break
     else:
         # If response is incorrect, subtract from score
-        player_score -= api_response[0]['value']
-        print('Oh, sorry! The correct response is "{}". You have ${}.\n'.format(api_response[0]['answer'], player_score))
+        #If API returns 'None' for clue value, catch TypeError
+        try:
+            player_score -= api_response[0]['value']
+            print('\nOh, sorry! The correct response is {}. You have ${}.\n'.format(api_response[0]['answer'].upper(), player_score))
+        except TypeError:
+            print("Whoops, the writers made an error. Let's try another clue.")
